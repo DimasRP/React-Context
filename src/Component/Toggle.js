864cb7a6cf5aa485f2ee1ef4df04410d02293c76
DimasRP <drizki80@gmail.com>
2022-08-29 20:00:25 +0700

    update css

--- src/Component/Toggle.js
+++ src/Component/Toggle.js
@@ -2,13 +2,16 @@ import React from 'react'
 import { useContext } from 'react';
 import { LayoutContext } from '../Context/LayoutContext';
 import { Button } from 'primereact/button';
+import { InputSwitch } from 'primereact/inputswitch';
+
 
 const Toggle = () => {
     const {nightmode, handleToggle} = useContext(LayoutContext)
     return ( 
         <div>
             <Button onClick={handleToggle}>Change {nightmode ? "day":"night"}</Button>
-            
+            <InputSwitch checked={handleToggle} >{nightmode ? "day":"night"}</InputSwitch>
+ 
         </div>
      );
 }
